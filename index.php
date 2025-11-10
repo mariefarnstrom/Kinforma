dae0c657909f8594d15ba37adf11d98424f228b3
Marie Färnström <fernstrm@hotmail.com>
2025-11-10 14:19:51 +0100

    Added Header, hero and navigation bar

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
     <title>Kinforma</title>
 </head>
 <body>
-    
+    <header>
+        <div class="banner">
+            <span>MISSA INTE CHANSEN LÄMNA DIN E-POST ADRESS</span>
+            <button class="closeButton"><img src="/assets/icons/x-icon.png"></button>
+        </div>
+        <h1>KIN/FORMA</h1>
+        <p>Riktig värme handlar aldrig om utsidan</p>
+    </header>
+    <nav class="navigationBar">
+        <a><img src="/assets/icons/search.png"></a>
+        <a><img src="/assets/icons/profile.png"></a>
+        <a><img src="/assets/icons/shop.png"></a>
+        <a><img src="/assets/icons/menu.png"></a>
+    </nav>
+    <main>
+        <article class="hero">
+            <video autoplay muted loop src="/assets/videos/grass.mp4"></video>
+        </article>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
 </body>
 </html>
