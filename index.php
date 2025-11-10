2cdd73d09df875218694b9073c9e3a10c1f9fb48
Marie Färnström <fernstrm@hotmail.com>
2025-11-10 13:28:45 +0100

    Added header, hero video and navigation bar

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/style.css">
     <title>Kinforma</title>
 </head>
+
 <body>
-    
+    <header>
+        <h1>KIN/FORMA</h1>
+        <p>Riktig värme handlar aldrig om utsidan</p>
+    </header>
+    <nav class="navigationBar">
+        <a><img src="/assets/icons/search.png"></a>
+        <a><img src="/assets/icons/profile.png"></a>
+        <a><img src="/assets/icons/shop.png"></a>
+        <a><img src="/assets/icons/menu.png"></a>
+    </nav>
+    <main>
+        <article class="hero">
+            <video autoplay muted loop src="/assets/videos/grass.mp4"></video>
+        </article>
+
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
+
 </html>
