7fb95e3cf78c6e8abf408030778bde518793fdd2
Marie Färnström <fernstrm@hotmail.com>
2025-11-11 13:30:12 +0100

    Added footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <nav class="aboutUs">
+       <a>OM KINFORMA</a>
+        <a>KONTAKTA OSS</a>
+    </nav>
+    <nav>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+    </nav>
+    <p>&copy copyright all rights reserved</p>
+</footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -27,13 +27,13 @@
         <article class="hero">
             <video autoplay muted loop src="/assets/videos/grass.mp4"></video>
         </article>
+        <section class="carousel">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
 
     </main>
-
-    <footer>
-
-    </footer>
-
-</body>
-
-</html>
+<?php 
+require __DIR__.'/footer.php';
