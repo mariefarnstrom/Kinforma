0c87f2ccc467d178cb5a88718ce2f4749236993b
Marie Färnström <fernstrm@hotmail.com>
2025-11-11 13:37:50 +0100

    added header.php

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/style.css">
-    <title>Kinforma</title>
-</head>
-
-<body>
-    <header>
-        <div class="banner">
-            <span>MISSA INTE CHANSEN LÄMNA DIN E-POST ADRESS</span>
-            <button class="closeButton"><img src="/assets/icons/x-icon.png"></button>
-        </div>
-        <h1>KIN/FORMA</h1>
-        <p>Riktig värme handlar aldrig om utsidan</p>
-    </header>
+require __DIR__.'/header.php';
+?>
     <nav class="navigationBar">
         <a><img src="/assets/icons/search.png"></a>
         <a><img src="/assets/icons/profile.png"></a>
