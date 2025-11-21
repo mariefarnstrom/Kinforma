e4f270b4ea30c740a7cf9730acd3e178548fcdf0
Marie Färnström <fernstrm@hotmail.com>
2025-11-21 15:30:39 +0100

    small change in media query for configurator

--- index.php
+++ index.php
@@ -55,7 +55,6 @@ require __DIR__ . '/header.php';
             </div>
         </div>
         <div id="config">
-        <!-- <section id="variantSelector"> -->
             <div class="variantHolder">
                 <img id="productVariant" src="assets/images/circle-red.png" /><br />
                 <p id="productInfo">TECH väska är utvecklad med KIN Tech/material</p>
@@ -66,7 +65,6 @@ require __DIR__ . '/header.php';
                 <div><img id="model3" class="modelButton" src="/assets/images/switchable-item(2).png" data-model="star"></div>
                 <div><img id="model4" class="modelbutton" src="/assets/images/switchable-item(3).png"></div>
             </section>
-        <!-- </section> -->
         <!-- Color buttons -->
         <div id="colors">
             <div id="redButton" class="colorButton" data-color="red"></div>
